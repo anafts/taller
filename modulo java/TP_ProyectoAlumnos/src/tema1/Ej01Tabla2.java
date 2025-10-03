2c59dc3fd8044076ca695f0e8b86cc1f58855f35
anafts <ana.freitaas16@gmail.com>
2025-10-03 20:48:07 -0300

    practica 1 java - eje 1 y 2

--- modulo java/TP_ProyectoAlumnos/src/tema1/Ej01Tabla2.java
+++ modulo java/TP_ProyectoAlumnos/src/tema1/Ej01Tabla2.java
@@ -3,21 +3,36 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tema1;
+    package tema1;
 
-public class Ej01Tabla2 {
+    import PaqueteLectura.GeneradorAleatorio;
 
-    /**
-     * Carga un vector que representa la tabla del 2
-     */
-    public static void main(String[] args) {
-        int DF=11;  
-        int [] tabla2 = new int[DF]; // indices de 0 a 10
-        int i;
-        for (i=0;i<DF;i++) 
-            tabla2[i]=2*i;
-        System.out.println("2x" + "5" + "="+ tabla2[5]);
+    public class Ej01Tabla2 {
+
+        /**
+         * Carga un vector que representa la tabla del 2
+         */
+        public static void main(String[] args) {
+            
+            GeneradorAleatorio.iniciar();
+            
+            int DF = 11;  
+            int [] tabla2 = new int[DF]; // indices de 0 a 10
+            
+            for (int i=0; i<DF ;i++) {
+                tabla2[i] = 2*i;
+            }
+      
+            int nroAleatorio = GeneradorAleatorio.generarInt(12);
+            
+            while (nroAleatorio != DF) {
+                
+                System.out.println("2 x " + nroAleatorio + " = "+ tabla2[nroAleatorio]);
+                
+                nroAleatorio = GeneradorAleatorio.generarInt(12);
+            }
+            
+
+        }
 
     }
-    
-}
--- modulo java/TP_ProyectoAlumnos/src/tema1/Ej02Jugadores.java
+++ modulo java/TP_ProyectoAlumnos/src/tema1/Ej02Jugadores.java
@@ -2,25 +2,48 @@
 package tema1;
 
 //Paso 1: Importar la funcionalidad para lectura de datos
+import PaqueteLectura.Lector;
 
 
 public class Ej02Jugadores {
-
   
     public static void main(String[] args) {
+        
+         int DF = 2;
+         
         //Paso 2: Declarar la variable vector de double 
+        double [] vector;
         
         //Paso 3: Crear el vector para 15 double 
+        vector = new double [DF];
         
         //Paso 4: Declarar indice y variables auxiliares a usar
+         double promedio, suma = 0; 
+         int cantidad = 0; 
                 
         //Paso 6: Ingresar 15 numeros (altura), cargarlos en el vector, ir calculando la suma de alturas
+        System.out.println("Escriba las alturas de los jugadores");
+        for(int i=0; i < DF; i++) {
+            double altura = Lector.leerDouble(); 
+            vector[i] = altura;
+            suma += altura;
+        }
         
         //Paso 7: Calcular el promedio de alturas, informarlo
+        promedio = suma / DF;
+                
+        
+        System.out.println("El promedio fue de: " + String.format("%.2f", promedio));
         
         //Paso 8: Recorrer el vector calculando lo pedido (cant. alturas que están por encima del promedio)
+        for(int i=0; i < DF; i++){
+            if(vector[i] > promedio) {
+                cantidad += 1;
+            }
+    }
      
         //Paso 9: Informar la cantidad.
+        System.out.println("La cantidad de jugadores con la altura mayor que " + String.format("%.2f", promedio) + " es " + cantidad);
     }
     
 }
